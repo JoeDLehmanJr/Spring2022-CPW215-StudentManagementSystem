cd19736225ecc1292d685d0e00bd53a92239c12a
JoeLehmanJr <joejr65@live.com>
2022-05-24 13:52:11 -0700

    Created the form to add a student.

---
+++ StudentManagementSystem/CreateStudentForm.cs
@@ -0,0 +1,34 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentManagementSystem
+{
+    public partial class FrmCreateStudentForm : Form
+    {
+        public FrmCreateStudentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCreateStudent_Click(object sender, EventArgs e)
+        {
+            // Assume data is valid
+
+            String firstName = TxtFirstName.Text;
+            String lastName = TxtLastName.Text;
+            DateTime dob = Convert.ToDateTime(TxtDOB.Text);
+
+            Student stu = new(firstName, lastName, dob);
+
+            StudentDb.add(stu);
+        }
+    }
+}
