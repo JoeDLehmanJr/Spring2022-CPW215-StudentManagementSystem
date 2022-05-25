9179e8909577acc26450ead9088e3ba6ebc4f117
JoeLehmanJr <joejr65@live.com>
2022-05-24 17:08:24 -0700

    Added a method to handle the connection string. Simplify the constructs to the new expression. Finished GetAllStudents method.

--- StudentManagementSystem/StudentDb.cs
+++ StudentManagementSystem/StudentDb.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿#nullable disable
+using System.Data.SqlClient;
 namespace StudentManagementSystem
 {
     static class StudentDb
@@ -9,12 +10,10 @@ namespace StudentManagementSystem
         /// <param name="s">The student to be added</param>
         public static void add(Student s)
         {
-            // Establish connection to database
-            SqlConnection con = 
-                new SqlConnection("Data Source = localhost; Initial Catalog = SMS; Integrated Security = True");
+            SqlConnection con = GetDatabaseConnection();
 
             // Prepare insert statement
-            SqlCommand insertCmd = new SqlCommand();
+            SqlCommand insertCmd = new();
             insertCmd.Connection = con;
 
             // Parameterized query
@@ -34,9 +33,50 @@ namespace StudentManagementSystem
             con.Close();
         }
 
+        private static SqlConnection GetDatabaseConnection()
+        {
+            // Establish connection to database
+            return new SqlConnection("Data Source = localhost; Initial Catalog = SMS; Integrated Security = True");
+        }
+
+        /// <summary>
+        /// Gets a list of all students ordered by last name in 
+        /// ascending order
+        /// </summary>
+        /// <returns>List of students sorted by last name</returns>
         public static List<Student> GetAllStudents() 
         { 
-             
+             // Get Connection
+             SqlConnection con = GetDatabaseConnection();
+
+            // Prepare the query
+            SqlCommand selCmd = new();
+            selCmd.Connection = con;
+            selCmd.CommandText = "Select StudentId,FName, LName, DateOfBirth" +
+                "  From Student Order By LName";
+            con.Open();
+            // Execute the query and use results
+            SqlDataReader reader = selCmd.ExecuteReader();
+
+            List<Student> students = new List<Student>();
+            while (reader.Read())
+            {
+                int id = Convert.ToInt32(reader["StudentId"]);
+                string FName = reader["FName"].ToString();
+                string LName = reader["LName"].ToString();
+                DateTime dob = Convert.ToDateTime(reader["DateOfBirth"]);
+
+                Student tempStu = new(FName, LName, dob);
+                tempStu.StudentId = id;
+
+                students.Add(tempStu);
+
+            }
+            // Close connection
+            con.Close();
+
+            // Return list of students
+            return students;
         }
 
         public static void Update(Student s) 
