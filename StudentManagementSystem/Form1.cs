851fcda393131b574206251782f217cb600f4d87
JoeLehmanJr <joejr65@live.com>
2022-05-30 16:23:33 -0700

    Added Update Student button and delete student button. Added try/Catch blocks to the BtnDeleteStudent click and the rest of the code for the delete button.

--- StudentManagementSystem/Form1.cs
+++ StudentManagementSystem/Form1.cs
@@ -1,4 +1,6 @@
 #nullable disable
+using System.Data.SqlClient;
+
 namespace StudentManagementSystem
 {
     public partial class Form1 : Form
@@ -10,7 +12,7 @@ namespace StudentManagementSystem
 
         private void BtnOpenCreateForm_Click(object sender, EventArgs e)
         {
-            FrmCreateStudentForm newStuForm = new();
+            FrmCreateOrUpdateStudentForm newStuForm = new();
             newStuForm.ShowDialog();
 
             PopulateStudentList();
@@ -35,8 +37,39 @@ namespace StudentManagementSystem
 
         private void LstStudents_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Student selectedStu = LstStudents.SelectedItem as Student;
-            MessageBox.Show(selectedStu.FullName + " " + selectedStu.DateOfBirth);
+            //Student selectedStu = LstStudents.SelectedItem as Student;
+            //MessageBox.Show(selectedStu.FullName + " " + selectedStu.DateOfBirth);
+        }
+
+        private void BtnDeleteStudent_Click(object sender, EventArgs e)
+        {
+            // End method if no student is selected
+            if (LstStudents.SelectedIndex == -1)
+            {
+                MessageBox.Show("You must select a student to delete.");
+                return;
+            }
+            Student selectedStudent = (Student)LstStudents.SelectedItem as Student;
+            try
+            {
+                StudentDb.Delete(selectedStudent);
+                MessageBox.Show($"{selectedStudent.FullName} deleted successfully");
+                PopulateStudentList();
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("That student no longer exists");
+                PopulateStudentList();
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("We are having server issues. Please try again later.");
+            }
+        }
+
+        private void BtnUpdateStudent_Click(object sender, EventArgs e)
+        {
+            FrmCreateOrUpdateStudentForm updateForm = new FrmCreateOrUpdateStudentForm();
         }
     }
 }
