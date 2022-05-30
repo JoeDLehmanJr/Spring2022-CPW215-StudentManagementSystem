ec7edeeae111e83e87cae9cd680e6b06010cc38b
JoeLehmanJr <joejr65@live.com>
2022-05-30 16:28:01 -0700

    Renamed form to fit with the new purpose of Create or Update.

---
+++ StudentManagementSystem/CreateOrUpdateStudentForm.cs
@@ -0,0 +1,36 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentManagementSystem
+{
+    public partial class FrmCreateOrUpdateStudentForm : Form
+    {
+        public FrmCreateOrUpdateStudentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCreateStudent_Click(object sender, EventArgs e)
+        {
+            // Assume data is valid
+
+            String firstName = TxtFirstName.Text;
+            String lastName = TxtLastName.Text;
+            DateTime dob = Convert.ToDateTime(TxtDOB.Text);
+
+            Student stu = new(firstName, lastName, dob);
+
+            StudentDb.add(stu);
+            MessageBox.Show("Student added successfully!");
+            Close();
+        }
+    }
+}
